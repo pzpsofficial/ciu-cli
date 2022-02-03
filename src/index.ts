2746988494b96bd35f9709fc28c289bf18be503f
Bartek <pzpsofficial@gmail.com>
2022-02-03 16:40:17 +0100

    chore: linters

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello');
+#! /usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('ciu')
+  .description('CLI that brings full power of caniuse.com into your terminal')
+  .version('0.1.0');
+
+program
+  .argument('query')
+  .description('Display info about property support')
+  .action((str, options) => {
+    console.log('Siema');
+  });
+
+program.parse();
